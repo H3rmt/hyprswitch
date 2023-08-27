d7d3006b25fd1b591eda5150217b8af4fe3acd84
Enrico Stemmer <100319043+h3rmt@users.noreply.github.com>
2023-08-27 22:26:06 +0200

    added switch functionality

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use clap::{Parser};
+use hyprland::data::{Client, Clients};
+use hyprland::prelude::*;
+use hyprland::{dispatch::*};
+use hyprland::dispatch::DispatchType::FocusWindow;
+use hyprland::shared::Address;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Switch between windows of same class
+    #[arg(long)]
+    same_class: bool,
+
+    /// Switch backwards
+    #[arg(long)]
+    reverse: bool,
+}
+
+///
+/// # Usage
+///
+/// * Switch between windows of same class
+///     * `window_switcher --same-class`
+/// * Switch backwards
+///     * `window_switcher --reverse`
+///
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Args::parse();
+
+    let mut clients = Clients::get()?
+        .filter(|c| c.workspace.id != -1)
+        .collect::<Vec<_>>();
+
+    clients.sort_by(|a, b| {
+        return if a.workspace.id != b.workspace.id {
+            a.workspace.id.cmp(&b.workspace.id)
+        } else {
+            a.at.0.cmp(&b.at.0)
+        };
+    });
+
+    let win = Client::get_active()?.expect("No active window?");
+    if cli.same_class {
+        clients = clients
+            .into_iter()
+            .filter(|c| c.class == win.class)
+            .collect::<Vec<_>>();
+    }
+
+    let mut current_window_index = clients.iter()
+        .position(|r| r.address.to_string() == win.address.to_string())
+        .expect("Active window not found?");
+
+    if cli.reverse {
+        current_window_index = if current_window_index == 0 { clients.len() - 1 } else { current_window_index - 1 };
+    } else {
+        current_window_index += 1;
+    }
+
+    let next_client = clients.iter()
+        .cycle()
+        .skip(current_window_index)
+        .next()
+        .expect("No next window?");
+
+
+    // removal of 0x prefix (hyprland adds it on dispatch)
+    let address = Address::new(
+        next_client.address.to_string()
+            .strip_prefix("0x")
+            .expect("0x could not be stripped")
+            .to_string()
+    );
+
+    Dispatch::call(FocusWindow(WindowIdentifier::Address(address)))?;
+
+    Ok(())
+}
