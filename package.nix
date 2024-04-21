f34265477cdb28eb1f204ede0a385da2e345fc35
Devin Droddy <devin.droddy@gmail.com>
2024-04-21 19:58:04 -0400

    finalize fromTOML usage + switch from cargoHash to cargoLock

--- package.nix
+++ package.nix
@@ -4,36 +4,35 @@
   pkg-config,
   rustPlatform,
   makeWrapper,
-}:
-let
-  meta = (builtins.fromTOML ./Cargo.toml).package;
+}: let
+  package = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
 in
-rustPlatform.buildRustPackage rec {
-  name = meta.name;
-  version = meta.version;
+  rustPlatform.buildRustPackage rec {
+    name = package.name;
+    version = package.version;
 
-  src = lib.cleanSource ./.;
+    src = lib.cleanSource ./.;
 
-  cargoHash = "sha256-LiH7OqQL7te1GVF3qfYVRQpAhQmsVGtgwhKhZorQp2k=";
+    cargoLock.lockFile = ./Cargo.lock;
 
-  nativeBuildInputs = [
-    pkg-config
-    makeWrapper
-  ];
+    nativeBuildInputs = [
+      pkg-config
+      makeWrapper
+    ];
 
-  buildInputs = with pkgs; [
-    glib
-    gtk4
-    gtk4-layer-shell
-  ];
+    buildInputs = with pkgs; [
+      glib
+      gtk4
+      gtk4-layer-shell
+    ];
 
-  postInstall = ''
-    wrapProgram $out/bin/${name}
-  '';
+    postInstall = ''
+      wrapProgram $out/bin/${name}
+    '';
 
-  meta = with lib; {
-    description = meta.description;
-    homepage = meta.repository;
-    license = licenses.mit;
-  };
-}
+    meta = with lib; {
+      description = package.description;
+      homepage = package.repository;
+      license = licenses.mit;
+    };
+  }
