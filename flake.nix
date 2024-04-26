c94f4335183120f677b343adcdd07aa7e97bdad3
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-04-26 10:52:59 +0000

    less systems

--- flake.nix
+++ flake.nix
@@ -8,25 +8,9 @@
       imports = [ ];
       systems =  [
         "aarch64-linux"
-        "armv5tel-linux"
-        "armv6l-linux"
-        "armv7a-linux"
-        "armv7l-linux"
         "i686-linux"
-        "loongarch64-linux"
-        "m68k-linux"
-        "microblaze-linux"
-        "microblazeel-linux"
-        "mips-linux"
-        "mips64-linux"
-        "mips64el-linux"
-        "mipsel-linux"
-        "powerpc64-linux"
-        "powerpc64le-linux"
         "riscv32-linux"
         "riscv64-linux"
-        "s390-linux"
-        "s390x-linux"
         "x86_64-linux"
       ];
       perSystem =
