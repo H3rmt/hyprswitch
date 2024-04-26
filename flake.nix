28f2d91f1b2b33d0dc65e40027b11210910b1045
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-04-26 10:43:56 +0000

    specified systems

--- flake.nix
+++ flake.nix
@@ -5,8 +5,30 @@
   };
   outputs = inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [];
-      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ ];
+      systems =  [
+        "aarch64-linux"
+        "armv5tel-linux"
+        "armv6l-linux"
+        "armv7a-linux"
+        "armv7l-linux"
+        "i686-linux"
+        "loongarch64-linux"
+        "m68k-linux"
+        "microblaze-linux"
+        "microblazeel-linux"
+        "mips-linux"
+        "mips64-linux"
+        "mips64el-linux"
+        "mipsel-linux"
+        "powerpc64-linux"
+        "powerpc64le-linux"
+        "riscv32-linux"
+        "riscv64-linux"
+        "s390-linux"
+        "s390x-linux"
+        "x86_64-linux"
+      ]
       perSystem =
         { config, pkgs, ... }: {
           packages.default = pkgs.callPackage ./package.nix { };
