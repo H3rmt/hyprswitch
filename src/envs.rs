979ade08f72e9e2212966878a8e301f603f8518b
Enrico Stemmer <enrico@h3rmt.zip>
2025-01-04 18:28:32 +0100

    fix: fix env loading
    Signed-off-by: Enrico Stemmer <enrico@h3rmt.zip>

--- src/envs.rs
+++ src/envs.rs
@@ -1,23 +1,28 @@
 use lazy_static::lazy_static;
 use log::debug;
+use std::env;
 
 lazy_static! {
-    pub static ref ICON_SIZE: i32 =
-        option_env!("ICON_SIZE").map_or(512, |s| s.parse().expect("Failed to parse ICON_SIZE"));
-    pub static ref SHOW_DEFAULT_ICON: bool = option_env!("SHOW_DEFAULT_ICON").map_or(false, |s| s
-        .parse()
-        .expect("Failed to parse SHOW_DEFAULT_ICON"));
-    pub static ref SHOW_LAUNCHER: bool = option_env!("SHOW_LAUNCHER")
-        .map_or(false, |s| s.parse().expect("Failed to parse SHOW_LAUNCHER"));
-    pub static ref LAUNCHER_MAX_ITEMS: usize = option_env!("LAUNCHER_MAX_ITEMS").map_or(5, |s| {
-        let value = s.parse().expect("Failed to parse LAUNCHER_MAX_ITEMS");
-        if !(1..=10).contains(&value) {
-            panic!("LAUNCHER_MAX_ITEMS must be between 1 and 10");
-        }
-        value
-    });
+    pub static ref ICON_SIZE: i32 = env::var("ICON_SIZE")
+        .map(|s| s.parse().expect("Failed to parse ICON_SIZE"))
+        .unwrap_or(512);
+    pub static ref SHOW_DEFAULT_ICON: bool = env::var("SHOW_DEFAULT_ICON")
+        .map(|s| s.parse().expect("Failed to parse SHOW_DEFAULT_ICON"))
+        .unwrap_or(false);
+    pub static ref SHOW_LAUNCHER: bool = env::var("SHOW_LAUNCHER")
+        .map(|s| s.parse().expect("Failed to parse SHOW_LAUNCHER"))
+        .unwrap_or(false);
+    pub static ref LAUNCHER_MAX_ITEMS: usize = env::var("LAUNCHER_MAX_ITEMS")
+        .map(|s| {
+            let value = s.parse().expect("Failed to parse LAUNCHER_MAX_ITEMS");
+            if !(1..=10).contains(&value) {
+                panic!("LAUNCHER_MAX_ITEMS must be between 1 and 10");
+            }
+            value
+        })
+        .unwrap_or(5);
     pub static ref DEFAULT_TERMINAL: Option<String> =
-        option_env!("DEFAULT_TERMINAL").map(|s| s.to_string());
+        env::var("DEFAULT_TERMINAL").map_or(None, |s| Some(s.to_string()));
 }
 
 pub fn envvar_dump() {
