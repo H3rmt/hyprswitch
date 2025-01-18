310cb87c9bd0d3a813223e1f9e52b896b4f21d07
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2025-01-19 03:42:16 +0700

    nix: format nix files

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,14 +1,23 @@
-{ self, lib, inputs }:
+{
+  self,
+  lib,
+  inputs,
+}:
 let
-  mkDate = longDate: (lib.concatStringsSep "-" [
-    (builtins.substring 0 4 longDate)
-    (builtins.substring 4 2 longDate)
-    (builtins.substring 6 2 longDate)
-  ]);
+  mkDate =
+    longDate:
+    (lib.concatStringsSep "-" [
+      (builtins.substring 0 4 longDate)
+      (builtins.substring 4 2 longDate)
+      (builtins.substring 6 2 longDate)
+    ]);
 
-  version = (lib.importTOML ../Cargo.toml).package.version
-    + "+date=" + (mkDate (self.lastModifiedDate or "19700101"))
-    + "_" + (self.shortRev or "dirty");
+  version =
+    (lib.importTOML ../Cargo.toml).package.version
+    + "+date="
+    + (mkDate (self.lastModifiedDate or "19700101"))
+    + "_"
+    + (self.shortRev or "dirty");
 in
 {
   default = self.overlays.hyprswitch;
